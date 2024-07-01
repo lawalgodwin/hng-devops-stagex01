050679c1a503e49b9422c6245310032e28c31f83
lawalgodwin <godwinl200@gmail.com>
2024-07-01 19:10:20 +0100

    Create user with password and save them to file

--- create_users.sh
+++ create_users.sh
@@ -1,6 +1,44 @@
 #!/usr/bin/env bash
 # A script that automates user and group management
 
+# generate random a password for users
+create_default_password () {
+    openssl rand -base64 12
+}
+
+# A function that creates a user
+create_user () {
+    username="$1"
+    # check if user already exists
+    if id "$username" &>/dev/null; then
+      echo "User: $username already exist"
+      return 1
+    else
+    # create the user if it does not exist
+      if useradd -m -U "$username"; then
+        echo "User: $username created successfully"
+        return 0
+      else
+    # could not creat user
+        echo "Error creating user: $username"
+        return 1
+      fi
+    fi
+}
+
+
+# A function that creates a user
+# create_group () {
+#     # check if the group already exist
+#     group="$1"
+#     if getent group "$group" &>/dev/null; then
+#       echo "Group: $group already exist"
+#     fi
+# }
+
+# set up the file for storing users and password
+mkdir -p /var/secure
+
 # Ensure the input file is supplied as argument to the script
 if [[ "$#" -lt 1 ]]; then
   echo "Usage: $0 INPUT_FILE"
@@ -14,7 +52,19 @@ if [[ -f "$input_file" ]]; then
   while read -r line; do
 # Extract the username and groups on every line
     username=$(echo "$line" | cut -d ";" -f 1)
-    usergroups=$(echo "$line" | cut -d ";" -f 2 | sed -e s/","/" "/g)
-    echo "$username ---> ${usergroups[*]}"
+    # usergroups=$(echo "$line" | cut -d ";" -f 2 | sed -e s/","/" "/g)
+    # create the user
+    create_user "$username"
+    # set user password and save user->password pairs to a file
+    password=$(create_default_password)
+    echo "$username:$password" | sudo chpasswd
+    echo "$username,$password" >> /var/secure/user_passwords.txt
+    # Set the file permission to read only for the owner
+    chmod 400 /var/secure/user_passwords.txt
+    # assign user to the respective group
+    # for group in "${usergroups[@]}"; do
+    #   echo ""
+    # done
   done < "$input_file"
 fi
+
